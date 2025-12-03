6dbf1b212f5351622c5584e6f079dd25cda25e12
A606\Jose <joracaam2006@gmail.com>
2025-12-03 21:13:15 +0100

    Subida ejercicio Mary Poppins

--- JAVA/src/main/java/org/example/MaryPoppins.java
+++ JAVA/src/main/java/org/example/MaryPoppins.java
@@ -1,4 +1,40 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class MaryPoppins {
-}
+
+    static void main() {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("------------------------------------------");
+
+        System.out.print("Introduce cuantas palabras quieres dar la vuelta: ");
+        int numero_vueltas = teclado.nextInt();
+
+        System.out.println("------------------------------------------");
+
+        for (int i = 0; i < numero_vueltas; i++) {
+
+            System.out.println("Introduce la palabra para darle la vuelta:");
+            String palabra = teclado.next();
+            String invertida = "";
+
+            for (int j = palabra.length()-1; j >= 0; j--) {
+                invertida += palabra.charAt(j);
+            }
+
+            System.out.println("Palabra invertida:");
+            System.out.println(invertida);
+
+            System.out.println("------------------------------------------");
+        }
+
+
+        }
+
+
+
+
+    }

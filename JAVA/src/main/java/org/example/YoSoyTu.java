65f3775a53f6989a8eb891ccd07dd9ff9e7cbb7d
A606\Jose <joracaam2006@gmail.com>
2025-12-03 20:58:36 +0100

    Subida ejercicio Yo Soy Tu ...

--- JAVA/src/main/java/org/example/YoSoyTu.java
+++ JAVA/src/main/java/org/example/YoSoyTu.java
@@ -1,4 +1,43 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class YoSoyTu {
+
+    static void main() {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("--------- PROGRAMA: Yo Soy Tu ... ---------");
+
+        System.out.print("Introduce cuantas pruebas se quieren realizar: ");
+        int num_pruebas = teclado.nextInt();
+
+        System.out.println("------------------------");
+
+        for (int i = 1; i < num_pruebas+1; i++) {
+
+            System.out.print("Nombre del PERSONAJE " + i + ": ");
+            String nombre = teclado.next();
+            System.out.print("Parentesco del PERSONAJE " + i + ": ");
+            String parentesco = teclado.next();
+
+            if (nombre.equals("Luke")  && parentesco.equals("padre")){
+                System.out.println("Resultado de la prueba:");
+                System.out.println("-> TOP SECRET");
+            }else {
+                System.out.println("Resultado de la prueba:");
+                System.out.println("-> " + nombre + ", yo soy tu " + parentesco);
+            }
+
+            System.out.println("------------------------");
+
+        }
+
+
+
+
+
+
+    }
 }

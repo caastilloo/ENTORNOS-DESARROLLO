8414987766a8a3ced90a09220d05a336e85f7644
Jose <josercaastilloo@gmail.com>
2026-03-03 15:59:04 +0100

    ++

---
+++ UD5/MutxaStoreSL/src/main/java/org/example/Cliente.java
@@ -0,0 +1,71 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Cliente {
+
+    private String idCliente;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private LocalDate fechaRegistro;
+
+    public Cliente(String idCliente, String apellidos, String nombre, String email, LocalDate fechaRegistro) {
+        this.idCliente = idCliente;
+        this.apellidos = apellidos;
+        this.nombre = nombre;
+        this.email = email;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "idCliente='" + idCliente + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", email='" + email + '\'' +
+                ", fechaRegistro=" + fechaRegistro +
+                '}';
+    }
+}
---
+++ UD5/MutxaStoreSL/src/main/java/org/example/Videojuego.java
@@ -0,0 +1,80 @@
+package org.example;
+
+public class Videojuego {
+
+    private String titulo;
+    private String plataforma;
+    private String genero;
+    private int anyoNacimiento;
+    private double precio;
+    private int stockDispo;
+
+    public Videojuego(String titulo, String plataforma, String genero, int anyoNacimiento, double precio, int stockDispo) {
+        this.titulo = titulo;
+        this.plataforma = plataforma;
+        this.genero = genero;
+        this.anyoNacimiento = anyoNacimiento;
+        this.precio = precio;
+        this.stockDispo = stockDispo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public int getAnyoNacimiento() {
+        return anyoNacimiento;
+    }
+
+    public void setAnyoNacimiento(int anyoNacimiento) {
+        this.anyoNacimiento = anyoNacimiento;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStockDispo() {
+        return stockDispo;
+    }
+
+    public void setStockDispo(int stockDispo) {
+        this.stockDispo = stockDispo;
+    }
+
+    @Override
+    public String toString() {
+        return "Videojuego{" +
+                "titulo='" + titulo + '\'' +
+                ", plataforma='" + plataforma + '\'' +
+                ", genero='" + genero + '\'' +
+                ", anyoNacimiento=" + anyoNacimiento +
+                ", precio=" + precio +
+                ", stockDispo=" + stockDispo +
+                '}';
+    }
+}
